af9bbf1125723c7efff507ad2df565a7e011370c
molor824 <molormargaderdene@gmail.com>
2026-07-23 17:16:42 +0800

    Added numbers

--- synthoza-editor/scripts/piano_roll/note_grid.gd
+++ synthoza-editor/scripts/piano_roll/note_grid.gd
@@ -1,6 +1,6 @@
 class_name NoteGrid extends Control
 
-const _FONT_SIZE := 20
+const _FONT_SIZE := 14
 const _PRIMARY_COLOR := Color(1, 1, 1, 0.3)
 const _SECONDARY_COLOR := Color(1, 1, 1, 0.15)
 const _SNAP_COLOR := Color(1, 1, 1, 0.05)
@@ -19,11 +19,14 @@ func _ready() -> void:
 	_piano_roll.time_signature_changed.connect(queue_redraw)
 
 func _draw() -> void:
-	var snap_per_note := maxi(_piano_roll.snap_value / _piano_roll.note_value, 1)
+	var snap_value := maxi(_piano_roll.snap_value, _piano_roll.note_value)
+	var snap_per_note := snap_value / _piano_roll.note_value
+	var snap_per_sig := snap_per_note * _piano_roll.notes
 	var note_size := _piano_roll.note_size
-	var snap_size := note_size / Vector2(maxi(_piano_roll.snap_value / 4, 1), 1)
+	var snap_size := note_size * Vector2(4.0 / float(snap_value), 1)
 	var cursor := -(_piano_roll.offset).posmodv(snap_size)
 	var grid_num := Vector2i(_piano_roll.offset / snap_size)
+	var font := get_theme_default_font()
 	
 	while cursor.y < size.y:
 		var y := size.y - cursor.y
@@ -38,7 +41,7 @@ func _draw() -> void:
 		cursor.y += snap_size.y
 	
 	while cursor.x < size.x:
-		var snap_signature := posmod(grid_num.x, _piano_roll.notes * snap_per_note)
+		var snap_signature := posmod(grid_num.x, snap_per_sig)
 		draw_line(
 			Vector2(cursor.x, 0), Vector2(cursor.x, size.y),
 			_PRIMARY_COLOR if snap_signature == 0 else (_SECONDARY_COLOR
@@ -47,6 +50,17 @@ func _draw() -> void:
 			-1,
 			true
 		)
+		if snap_signature == 0:
+			var signature_num := int(grid_num.x / snap_per_sig) + 1
+			draw_string(
+				font,
+				Vector2(cursor.x + 4, _FONT_SIZE),
+				str(signature_num),
+				HORIZONTAL_ALIGNMENT_LEFT,
+				-1,
+				_FONT_SIZE
+			)
+		
 		grid_num.x += 1
 		cursor.x += snap_size.x
 
--- synthoza-editor/scripts/piano_roll/piano.gd
+++ synthoza-editor/scripts/piano_roll/piano.gd
@@ -1,7 +1,9 @@
 class_name Piano extends Control
 
+const _FONT_SIZE := 14
+
 const _OUTLINE_WIDTH := 1.0
-const _BLACK_KEY_RATIO := 0.7
+const _BLACK_KEY_RATIO := 0.6
 const _BLACK_KEY_COLOR := Color(0.1, 0.1, 0.1)
 const _WHITE_KEY_COLOR := Color(1, 1, 1)
 const _OUTLINE_COLOR := Color.BLACK
@@ -15,11 +17,11 @@ const _WHITE_KEY_RECTS: Array[Rect2] = [
 	Rect2(0, 10.5, 1, 1.5)
 ]
 const _BLACK_KEY_RECTS: Array[Rect2] = [
-	Rect2(1 - _BLACK_KEY_RATIO, 1, _BLACK_KEY_RATIO, 1),
-	Rect2(1 - _BLACK_KEY_RATIO, 3, _BLACK_KEY_RATIO, 1),
-	Rect2(1 - _BLACK_KEY_RATIO, 5, _BLACK_KEY_RATIO, 1),
-	Rect2(1 - _BLACK_KEY_RATIO, 8, _BLACK_KEY_RATIO, 1),
-	Rect2(1 - _BLACK_KEY_RATIO, 10, _BLACK_KEY_RATIO, 1)
+	Rect2(0, 1, _BLACK_KEY_RATIO, 1),
+	Rect2(0, 3, _BLACK_KEY_RATIO, 1),
+	Rect2(0, 5, _BLACK_KEY_RATIO, 1),
+	Rect2(0, 8, _BLACK_KEY_RATIO, 1),
+	Rect2(0, 10, _BLACK_KEY_RATIO, 1)
 ]
 
 @export var _piano_roll: PianoRoll
@@ -43,19 +45,34 @@ func _draw() -> void:
 	var octave_size := key_size.y * 12
 	var cursor := -fposmod(_piano_roll.offset.y, octave_size)
 	var octave := int(_piano_roll.offset.y / octave_size)
+	var font := get_theme_default_font()
 	
 	while cursor <= size.y:
+		var offset := Vector2.DOWN * (size.y - cursor - octave_size)
 		for rect in _WHITE_KEY_RECTS:
 			_draw_key(Rect2(
-				rect.position * key_size + Vector2.DOWN * (size.y - cursor - octave_size),
+				rect.position * key_size + offset,
 				rect.size * key_size
 			), _WHITE_KEY_COLOR)
 		for rect in _BLACK_KEY_RECTS:
 			_draw_key(Rect2(
-				rect.position * key_size + Vector2.DOWN * (size.y - cursor - octave_size),
+				rect.position * key_size + offset,
 				rect.size * key_size
 			), _BLACK_KEY_COLOR)
+		
+		var width := font.get_string_size(str(octave))
+		draw_string(
+			font,
+			Vector2(key_size.x - width.x, key_size.y * 12 - 4) + offset,
+			str(octave),
+			HORIZONTAL_ALIGNMENT_LEFT,
+			-1,
+			_FONT_SIZE,
+			Color.BLACK
+		)
+		
 		cursor += octave_size
+		octave += 1
 
 func _gui_input(event: InputEvent) -> void:
 	var mouse_motion := event as InputEventMouseMotion
