3161b7dcf9d5d46f2a91f9bdc3711af7d469fa8f
molor824 <molormargaderdene@gmail.com>
2026-08-24 00:44:18 +0800

    Add mouse input handling for piano

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -3,44 +3,52 @@ use iced::{
     widget::{Canvas, scrollable},
 };
 
-use crate::piano::{Note, Piano};
+use crate::piano::{Piano, PianoMsg};
 
 mod piano;
 
 struct State {
-    notes: Vec<bool>,
-    note_height: f32,
+    keys: Vec<bool>,
+    key_height: f32,
 }
 
 #[derive(Clone)]
 enum Msg {
-    NoteUpdate(Note, bool),
+    PianoMsg(PianoMsg),
 }
 
 impl State {
     fn new() -> Self {
         Self {
-            notes: vec![false; 12 * 10],
-            note_height: 15.0,
+            keys: vec![false; 12 * 10],
+            key_height: 15.0,
         }
     }
     fn update(&mut self, msg: Msg) {
         match msg {
-            Msg::NoteUpdate(note, state) => self.notes[note.0 as usize] = state,
+            Msg::PianoMsg(msg) => match msg {
+                PianoMsg::KeyAdd(key) => self.keys[key] = true,
+                PianoMsg::KeyRemove(key) => self.keys[key] = false,
+                PianoMsg::KeyChange(from, to) => {
+                    self.keys[from] = false;
+                    self.keys[to] = true;
+                }
+            },
         }
     }
     fn view(&self) -> impl Into<Element<'_, Msg>> {
-        let piano = Canvas::new(Piano {
-            notes: &self.notes,
-            updater: Msg::NoteUpdate,
-        })
-        .height(self.notes.len() as f32 * self.note_height)
-        .width(Length::Fill);
+        let piano = Canvas::new(Piano { keys: &self.keys })
+            .height(self.keys.len() as f32 * self.key_height)
+            .width(Length::Fill);
 
-        scrollable(piano)
-            .anchor_bottom()
-            .height(Length::Fill)
-            .width(100.0)
+        Element::map(
+            scrollable(piano)
+                .anchor_bottom()
+                .height(Length::Fill)
+                .width(100.0)
+                .into(),
+            Msg::PianoMsg,
+        )
     }
 }
 
--- editor/src/piano.rs
+++ editor/src/piano.rs
@@ -1,13 +1,14 @@
 use std::mem::transmute;
 
 use iced::{
-    Color, Point, Size,
-    widget::canvas::{self, Frame, Stroke},
+    Color, Event, Point, Rectangle, Renderer, Size, Theme,
+    mouse::{self, Cursor},
+    widget::canvas::{Action, Frame, Geometry, Program, Stroke},
 };
 
-const BLACK_KEY_COL: f32 = 0.15;
-const ACTIVE_BLACK_KEY_COL: f32 = 0.2;
-const WHITE_KEY_COL: f32 = 0.85;
+const BLACK_KEY_COL: f32 = 0.1;
+const ACTIVE_BLACK_KEY_COL: f32 = 0.25;
+const WHITE_KEY_COL: f32 = 0.95;
 const ACTIVE_WHITE_KEY_COL: f32 = 0.8;
 const BLACK_KEY_WIDTH: f32 = 0.6;
 
@@ -59,70 +60,143 @@ const KEY_WIDTH_SCALES: [f32; 12] = [
 ];
 const KEY_RENDER_ORDER: [usize; 12] = [0, 2, 4, 5, 7, 9, 11, 1, 3, 6, 8, 10];
 
-pub struct Piano<'a, Msg, Upd>
-where
-    Upd: FnOnce(Note, bool) -> Msg,
-{
-    pub notes: &'a [bool], // Note maps. This also determines the piano length
-    pub updater: Upd,      // Updater function that updates piano according to message
+#[derive(Clone)]
+pub enum PianoMsg {
+    KeyAdd(usize),
+    KeyRemove(usize),
+    KeyChange(usize, usize),
 }
 
-impl<'a, Msg, Upd> canvas::Program<Msg> for Piano<'a, Msg, Upd>
-where
-    Upd: FnOnce(Note, bool) -> Msg,
-{
-    type State = ();
+pub struct Piano<'a> {
+    pub keys: &'a [bool], // Key maps. This also determines the piano length
+}
+
+#[derive(Default, Debug)]
+pub struct PianoState {
+    clicked_key: Option<usize>,
+    mouse_clicked: bool,
+}
+
+impl<'a> Piano<'a> {
+    fn note_rect(&self, key_size: Size, index: usize) -> Rectangle {
+        let key = index % 12;
+        Rectangle::new(
+            Point::new(
+                0.0,
+                ((self.keys.len() - index - 1) as f32 + KEY_OFFSETS[key]) * key_size.height,
+            ),
+            Size::new(
+                key_size.width * KEY_WIDTH_SCALES[key],
+                key_size.height * KEY_HEIGHT_SCALES[key],
+            ),
+        )
+    }
+    fn key_size(&self, bounds: Rectangle) -> Size {
+        Size::new(bounds.width, bounds.height / self.keys.len() as f32)
+    }
+}
+
+impl<'a> Program<PianoMsg> for Piano<'a> {
+    type State = PianoState;
     fn draw(
         &self,
         _state: &Self::State,
-        renderer: &iced::Renderer,
-        _theme: &iced::Theme,
-        bounds: iced::Rectangle,
-        _cursor: iced::mouse::Cursor,
-    ) -> Vec<canvas::Geometry<iced::Renderer>> {
+        renderer: &Renderer,
+        _theme: &Theme,
+        bounds: Rectangle,
+        _cursor: Cursor,
+    ) -> Vec<Geometry<Renderer>> {
         let mut frame = Frame::new(renderer, bounds.size());
-        let note_count = self.notes.len();
-        let key_height = bounds.height / note_count as f32;
+        let key_size = self.key_size(bounds);
 
         frame.fill_rectangle(Point::ORIGIN, bounds.size(), Color::BLACK);
 
-        for i in (0..note_count).step_by(12) {
-            for n in 0..12 {
-                let key = KEY_RENDER_ORDER[n];
+        for i in (0..self.keys.len()).step_by(12) {
+            for key in KEY_RENDER_ORDER {
                 let abs_key = key + i;
-                let Some(&active) = self.notes.get(abs_key) else {
+                let Some(&active) = self.keys.get(abs_key) else {
                     continue;
                 };
 
-                let start = Point::new(
-                    0.0,
-                    ((note_count - abs_key - 1) as f32 + KEY_OFFSETS[key]) * key_height,
-                );
-                let size = Size::new(
-                    bounds.width * KEY_WIDTH_SCALES[key],
-                    key_height * KEY_HEIGHT_SCALES[key],
-                );
-                let intensity = if active {
+                let rect = self.note_rect(key_size, abs_key);
+                let intensity = if !active {
                     KEY_COLS[key]
                 } else {
                     ACTIVE_KEY_COLS[key]
                 };
                 frame.fill_rectangle(
-                    start,
-                    size,
+                    rect.position(),
+                    rect.size(),
                     Color::from_rgb(intensity, intensity, intensity),
                 );
-                frame.stroke_rectangle(start, size, Stroke::default().with_width(2.0));
+                frame.stroke_rectangle(
+                    rect.position(),
+                    rect.size(),
+                    Stroke::default().with_width(2.0),
+                );
             }
         }
 
         vec![frame.into_geometry()]
     }
+
+    fn update(
+        &self,
+        state: &mut Self::State,
+        event: &Event,
+        bounds: Rectangle,
+        cursor: Cursor,
+    ) -> Option<Action<PianoMsg>> {
+        let Event::Mouse(mouse_event) = event else {
+            return None;
+        };
+
+        match mouse_event {
+            mouse::Event::ButtonPressed(mouse::Button::Left) => state.mouse_clicked = true,
+            mouse::Event::ButtonReleased(mouse::Button::Left) => state.mouse_clicked = false,
+            _ => {}
+        }
+
+        let mut clicked_key: Option<usize> = None;
+        let key_size = self.key_size(bounds);
+
+        if state.mouse_clicked
+            && let Cursor::Available(cursor_pos) = cursor
+        {
+            'outer: for i in (0..self.keys.len()).step_by(12) {
+                for key in KEY_RENDER_ORDER.into_iter().rev() {
+                    let abs_key = key + i;
+                    let note_rect = self.note_rect(key_size, abs_key);
+
+                    if note_rect.contains(cursor_pos) {
+                        clicked_key = Some(abs_key);
+                        break 'outer;
+                    }
+                }
+            }
+        }
+
+        let msg = match clicked_key {
+            Some(key) => match state.clicked_key {
+                Some(state_key) if state_key == key => None,
+                Some(state_key) => Some(PianoMsg::KeyChange(state_key, key)),
+                None => Some(PianoMsg::KeyAdd(key)),
+            },
+            None => match state.clicked_key {
+                Some(state_key) => Some(PianoMsg::KeyRemove(state_key)),
+                None => None,
+            },
+        };
+
+        state.clicked_key = clicked_key;
+
+        msg.map(|msg| Action::publish(msg))
+    }
 }
 
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub enum NoteKind {
+pub enum Note {
     C,
     CSharp,
     D,
@@ -136,24 +210,24 @@ pub enum NoteKind {
     ASharp,
     B,
 }
-impl TryFrom<u8> for NoteKind {
+impl TryFrom<u8> for Note {
     type Error = String;
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         if value >= 12 {
-            return Err(format!("value {value} cannot be converted to note kind"));
+            return Err(format!("value {value} cannot be converted to note"));
         }
         Ok(unsafe { transmute(value) })
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub struct Note(pub u32);
-impl Note {
-    pub const fn new(kind: NoteKind, octave: u32) -> Self {
-        Self(octave * 12 + kind as u8 as u32)
+pub struct Key(pub u32);
+impl Key {
+    pub const fn new(note: Note, octave: u32) -> Self {
+        Self(octave * 12 + note as u8 as u32)
     }
-    pub fn kind(self) -> NoteKind {
-        NoteKind::try_from((self.0 % 12) as u8).unwrap()
+    pub fn note(self) -> Note {
+        Note::try_from((self.0 % 12) as u8).unwrap()
     }
     pub fn octave(self) -> u32 {
         self.0 / 12
@@ -162,7 +236,7 @@ impl Note {
         2.0_f32.powf(self.0 as f32 / 12.0) * c0_freq
     }
     pub fn pitch_a4_freq(self, a4_freq: f32) -> f32 {
-        2.0_f32.powf((self.0 - Self::new(NoteKind::A, 4).0) as f32 / 12.0) * a4_freq
+        2.0_f32.powf((self.0 - Self::new(Note::A, 4).0) as f32 / 12.0) * a4_freq
     }
     pub fn pitch_standard(self) -> f32 {
         self.pitch_a4_freq(440.0)
